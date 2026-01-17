0a4a0ad741533be48f2fa536e9b760a0efb4adfc
Raj <rajudaniyaraj@gmail.com>
2026-01-17 18:14:10 +0000

    eighth problem solution

---
+++ h1_calc.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	int m;
+	cin >> n >> m;
+
+	cout << n << " + " << m << " = " << n + m << endl;
+	cout << n << " - " << m << " = " << n - m << endl;
+	cout << n << " * " << m << " = " << n * m << endl;
+	cout << n << " / " << m << " = " << n / m << endl;
+	cout << n << " % " << m << " = " << n % m << endl;	
+}
