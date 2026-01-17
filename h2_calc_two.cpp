3648b326275b301527c275c002c083439e620ba2
Raj <rajudaniyaraj@gmail.com>
2026-01-17 18:24:21 +0000

    ninth problem solution

---
+++ h2_calc_two.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	long n;
+	long m;
+	cin >> n;
+	cin >> m;
+
+	cout << n << " + " << m << " = " << n + m << endl << endl;
+	cout << n << " - " << m << " = " << n - m << endl << endl;
+	cout << n << " * " << m << " = " << n * m << endl << endl;
+	cout << n << " / " << m << " = " << n / m << endl << endl;
+	cout << n << " % " << m << " = " << n % m << endl;	
+}
