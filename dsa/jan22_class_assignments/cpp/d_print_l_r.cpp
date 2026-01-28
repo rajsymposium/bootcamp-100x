7afb8a7bcc6071236b0bb4029f542e78ecea2be0
Raj <rajudaniyaraj@gmail.com>
2026-01-28 09:29:42 +0000

    fourth problem solution

---
+++ dsa/jan22_class_assignments/cpp/d_print_l_r.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int left, right;
+    cin >> left >> right;
+
+    int i = left;
+    while (i <= right) {
+        cout << i << " ";
+        ++i;
+    }
+
+}
