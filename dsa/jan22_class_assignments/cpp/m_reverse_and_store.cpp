5592bbead8f0325d4402608729061fb4b4d980cb
Raj <rajudaniyaraj@gmail.com>
2026-01-28 10:39:27 +0000

    thirteenth problem solution

---
+++ dsa/jan22_class_assignments/cpp/m_reverse_and_store.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long num;
+    cin >> num;
+    long long reverse = 0;
+    do {
+        reverse = reverse*10 + (num % 10);
+        num /= 10;
+    } while (num > 0);
+
+    cout << reverse << endl;
+}
