cdd6c05775f07944e69af8fd49e5c9e99075cf4a
Raj <rajudaniyaraj@gmail.com>
2026-01-28 09:04:03 +0000

    first problem solution

---
+++ dsa/jan22_class_assignments/cpp/a_print_1_n.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cin >> num;
+
+    int i = 1;
+    while ( i <= num) {
+        cout << i << endl;
+        ++i;
+    }
+}
