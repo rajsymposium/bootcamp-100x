ab9661fef69a92434b9a8bf9cd6df69229606a4d
Raj <rajudaniyaraj@gmail.com>
2026-01-28 09:25:25 +0000

    third problem solution

---
+++ dsa/jan22_class_assignments/cpp/c_print_even_n.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cin >> num;
+
+    int i = 2;
+    while (i <= num) {
+        cout << i << " ";
+        i += 2;
+    }
+}
