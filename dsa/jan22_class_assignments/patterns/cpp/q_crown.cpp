88be8e2fa620acf3678161523f5060bc5fdcbcfa
Raj <rajudaniyaraj@gmail.com>
2026-01-29 17:07:24 +0000

    seventeenth problem solution

---
+++ dsa/jan22_class_assignments/patterns/cpp/q_crown.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows;
+    cin >> rows;
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j <= i; ++j) {
+            cout << "*";
+        }
+        for (int j = 0; j < 2 * (rows - i - 1); ++j) {
+            cout << " ";
+        }
+        for (int j = 0; j <= i; ++j) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
