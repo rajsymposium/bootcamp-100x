b778f04d5bf66bf99e90f0a5cbf2e2a9a65fb146
Raj <rajudaniyaraj@gmail.com>
2026-01-29 17:01:20 +0000

    fourteenth problem solution

---
+++ dsa/jan22_class_assignments/patterns/cpp/n_inverted_hollow_triangle.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows;
+    cin >> rows;
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < i; ++j) {
+            cout << " ";
+        }
+        for (int j = 0; j < rows - i; ++j) {
+            if (j == 0 || j == rows - i - 1 || i == 0) {
+                cout << "* ";
+            } else {
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+}
