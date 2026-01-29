18a95bc376819e994426bd5d3b9c6956b6f99693
Raj <rajudaniyaraj@gmail.com>
2026-01-29 16:46:33 +0000

    fourth problem solution

---
+++ dsa/jan22_class_assignments/patterns/cpp/d_hollow_square.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows;
+    cin >> rows;
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < rows; ++j) {
+            if (i == 0 || i == rows - 1 || j == 0 || j == rows - 1) {
+                cout << '*';
+            } else {
+                cout << ' ';
+            }
+        }
+        cout << endl;
+    }
+}
