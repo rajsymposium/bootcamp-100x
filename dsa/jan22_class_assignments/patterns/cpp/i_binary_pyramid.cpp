60bddf0908af667ef2b59141147e1ac17fdcbe2c
Raj <rajudaniyaraj@gmail.com>
2026-01-29 16:56:13 +0000

    ninth problem solution

---
+++ dsa/jan22_class_assignments/patterns/cpp/i_binary_pyramid.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows;
+    cin >> rows;
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j <= i; ++j) {
+            cout << (j + i) % 2;
+        }
+        cout << endl;
+    }
+}
